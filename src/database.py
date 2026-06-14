5bdc9dc38cad3d42a93f13496fe4800c8df497ab
Rijin Shaji <rijinshajiedathil@gmail.com>
2026-06-14 13:10:42 +0530

    Update database.py

--- src/database.py
+++ src/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
 
 DATABASE_URL = "sqlite:///./orders.db"
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
